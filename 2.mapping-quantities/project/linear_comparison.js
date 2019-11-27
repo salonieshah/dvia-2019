e41c106f9177395348f7f8e48450c152fce3791a
EC2 Default User <ec2-user@ip-172-31-29-143.ec2.internal>
2019-11-27 17:38:33 +0000

    commit

--- 2.mapping-quantities/project/linear_comparison.js
+++ 2.mapping-quantities/project/linear_comparison.js
@@ -33,7 +33,7 @@ function setup(){
 
 //4. Create Canvas  
   x= windowWidth;
-  y = windowHeight;
+  y = 35000;
   createCanvas(x, y);
   background(51)
 
@@ -43,12 +43,29 @@ function setup(){
 // for(let i=0;i<health;i++){
 //   rect(0.28*x,5+10*i, 5,10)
 // }
-
+  fill('#9C0D38'); 
  for(let i=0;i<nuc;i++){
-   rect(0.14*x,5+10*i, 5,10)
+   rect(0.7*x,5+5*i, 5,5)
  } 
+ fill('#EC5766');
  for(let i=0;i<health;i++){
-   rect(0.28*x,5+10*i, 5,10)
+   rect(0.14*x,5+5*i, 5,5)
+ }
+ fill('#9C0D38');
+  for(let i=0;i<food;i++){
+   rect(0.21*x,5+5*i, 5,5)
+ } 
+ fill('#FCC631');
+ for(let i=0;i<tree;i++){
+   rect(0.28*x,5+5*i, 5,5)
+ }
+ fill('#FDED7C');
+  for(let i=0;i<shelter;i++){
+   rect(0.35*x,5+5*i, 5,5)
+ }
+ fill('#FCBA04');
+ for(let i=0;i<education;i++){
+   rect(0.42*x,5+5*i, 5,5)
  }
   
   
